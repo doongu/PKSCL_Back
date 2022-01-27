439f77bfb557be1989e212062b70bd75aedc9c93
Junkyu Han <must1080@naver.com>
2022-01-27 19:38:59 +0900

    Update WebConfig.java

--- src/main/java/com/example/pkscl/config/WebConfig.java
+++ src/main/java/com/example/pkscl/config/WebConfig.java
@@ -5,7 +5,7 @@ import com.example.pkscl.Interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+  
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
     
