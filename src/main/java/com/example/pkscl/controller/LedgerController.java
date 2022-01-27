097a1349efa05dc05a34dbf694bb906c4d7e6688
JoHyeonseok <wh9832@naver.com>
2022-01-27 18:46:22 +0900

    Chore: getStudentData stdId -> stdID로 변경

--- src/main/java/com/example/pkscl/controller/LedgerController.java
+++ src/main/java/com/example/pkscl/controller/LedgerController.java
@@ -1,8 +1,10 @@
 package com.example.pkscl.controller;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.example.pkscl.service.LedgerService;
 
@@ -23,9 +25,41 @@ public class LedgerController {
     }
 
     @GetMapping(value = "/ledger")
-    public ResponseEntity<LinkedHashMap<String, Object>> pkscl(HttpServletRequest request) {
+    public Map<String, Object> getLedger(HttpServletRequest request, HttpServletResponse response) {
         String majorNumber = (String) request.getSession(false).getAttribute("majorNumber");
-        return new ResponseEntity<>(ledgerService.getMajorData(majorNumber), HttpStatus.OK);
+        return new LinkedHashMap<String, Object>() {
+            {
+                put("ledger", ledgerService.getLedger(majorNumber));
+            }
+        };
     }
 
+    @GetMapping(value = "/ledger/admin")
+    public Map<String, Object> getLedgerAdmin(HttpServletRequest request, HttpServletResponse response) {
+        String position = (String) request.getSession(false).getAttribute("position");
+        return new ResponseEntity<>(ledgerService.getAdminData(position), HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/ledger")
+    public void patchLedger(@RequestBody Map<String, Object> body, HttpServletRequest request, HttpServletResponse response) {
+        String position = (String) request.getSession(false).getAttribute("position");
+        String majorNumber = (String) request.getSession(false).getAttribute("majorNumber");
+        String patchStatus = (String) body.get("status");
+        List<String> emailList = (List<String>) body.get("email");
+
+        // 400 Bad Request
+        if (emailList == null || emailList.size() == 0 || patchStatus == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // 403 Forbidden
+        if (!position.equals("president")) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+
+        // 서비스 호출 및 반환
+        ledgerService.patchLedger(majorNumber, patchStatus, emailList);
+    }
 }
